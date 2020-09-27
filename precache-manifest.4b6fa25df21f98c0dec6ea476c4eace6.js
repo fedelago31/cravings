a7dfb0452bfdf495ea0b53e9b31e2fd291105bd1
fedelago31 <fede.lagomarsino@gmail.com>
2020-09-27 12:49:28 +0200

    Updates

---
+++ precache-manifest.4b6fa25df21f98c0dec6ea476c4eace6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b383ef9b6a001240d328712ef73525",
+    "url": "/cravings/index.html"
+  },
+  {
+    "revision": "857c67547a44027bd840",
+    "url": "/cravings/static/css/main.8f9257d1.chunk.css"
+  },
+  {
+    "revision": "4489091e827413e13dc0",
+    "url": "/cravings/static/js/2.a989d74c.chunk.js"
+  },
+  {
+    "revision": "e6191615acbaa88870aef4cd0368e4ec",
+    "url": "/cravings/static/js/2.a989d74c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "857c67547a44027bd840",
+    "url": "/cravings/static/js/main.0aa8826c.chunk.js"
+  },
+  {
+    "revision": "5616cfdbe2b400653492",
+    "url": "/cravings/static/js/runtime-main.cc267968.js"
+  }
+]);
